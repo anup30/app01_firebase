d78a08fce2e31a718228a8a813116451f7f6b7ee
anup30 <anupbarua30@gmail.com>
2024-04-03 02:14:37 +0600

    firebase, movie_list 1.1

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
 
-// firebase class2, on time at: 11:00
+// firebase class2, on time at: 32:00
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized(); // <-- for firebase
   await Firebase.initializeApp(
@@ -25,3 +25,19 @@ class MoviesApp extends StatelessWidget {
     );
   }
 }
+
+//the (my) database is in test mode(anyone can modify)-------
+
+/*
+for error:
+ERROR:D8: Cannot fit requested classes in a single dex file (# methods: 94247 > 65536)
+com.android.builder.dexing.DexArchiveMergerException: Error while merging dex archives:
+The number of method references in a .dex file cannot exceed 64K. .....
+
+
+in project>app>src>build.gradle file:
+1) in defaultConfig {}
+add, multiDexEnabled true
+2) in dependencies {}
+add, implementation 'com.android.support:multidex:1.0.3'
+*/
--- lib/movie_list_screen.dart
+++ lib/movie_list_screen.dart
@@ -1,24 +1,71 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class MovieListScreen extends StatelessWidget {
+class MovieListScreen extends StatefulWidget {
   const MovieListScreen({super.key});
 
+  @override
+  State<MovieListScreen> createState() => _MovieListScreenState();
+}
+
+class _MovieListScreenState extends State<MovieListScreen> {
+  final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance; // https://firebase.google.com/docs/storage/flutter/start
+  final List<Movie> movieList =[];
+  @override
+  void initState() {
+    super.initState();
+    _getMovieList();
+  }
+  void _getMovieList(){
+    _firebaseFirestore.collection('movies').get().then((value){ // <--- where from ? , class 2, Getting all documents from one collection
+      //print(value); //Instance of '_JsonQuerySnapshot'
+      movieList.clear();
+      for(QueryDocumentSnapshot doc in value.docs){ // or var doc, class 2 <---
+        print(doc.data()); // prints my firestore 'movies' data
+        movieList.add(
+            Movie.fromJson(doc.id, doc.data() as Map<String,dynamic>)
+        );
+      }
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Movies"),backgroundColor: Colors.blue),
       body: ListView.separated(
-        itemCount: 10,
+        itemCount: movieList.length,
         itemBuilder: (context,index){
           return ListTile(
-            title: Text("Movie Name"),
-            subtitle: Text("Languages"),
-            leading: Text("1.4"),
-            trailing: Text("2021"),
+            title: Text(movieList[index].name),
+            subtitle: Text(movieList[index].languages),
+            leading: Text(movieList[index].rating),
+            trailing: Text(movieList[index].year),
           );
         },
         separatorBuilder:(_,__)=> const Divider(),
       ),
+    );
+  }
+}
+
+class Movie{  // model/pojo class
+  final String id, name, languages, year, rating, earning;
+  Movie({
+    required this.id,
+    required this.name,
+    required this.languages,
+    required this.year,
+    required this.rating,
+    required this.earning,
+  });
+  factory Movie.fromJson(String id, Map<String, dynamic> json){
+    return Movie(
+        id: id,
+        name: json['name'],
+        languages: json['languages'],
+        year: json['year'],
+        rating: json['rating'],
+        earning: json['earning']??'Unknown',
     );
   }
 }
