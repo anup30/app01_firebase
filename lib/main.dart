962d5ad52bc5c5b70d79a4044f734f12953119d5
anup30 <anupbarua30@gmail.com>
2024-04-02 18:38:23 +0600

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
-// random Color
-// https://stackoverflow.com/questions/51340588/flutter-how-can-i-make-a-random-color-generator-background
-// issue: color flickering on hot reload, for function usage.
+// added to firebase project
+// random Color // use stream/concurrency for large ListTile ?
+// issue: color flickering on hot reload, for function usage. build gets called twice without setState (v2, with solution)
+// see also: https://stackoverflow.com/questions/64834702/why-build-function-gets-called-twice-when-hot-restarting
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // <-- for firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -27,15 +35,29 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   //Color myColor=randColor();
+  Color val = Colors.primaries[math.Random().nextInt(Colors.primaries.length)].withOpacity(0.5);
+  //to fix the flickering error get color1, color2, color3 here, before build -------------
+  List<Color> lc=[];
+  void setColors(){
+    lc =
+    [ // can use list generate for many colors
+      Colors.primaries[math.Random().nextInt(Colors.primaries.length)].withOpacity(0.8), // has transparency? check in a Stack, with background image.
+      Colors.primaries[math.Random().nextInt(Colors.primaries.length)].withOpacity(0.8),
+      Colors.primaries[math.Random().nextInt(Colors.primaries.length)].withOpacity(0.8),
+    ];
+  }
+  @override
+  void initState() {
+    super.initState();
+    setColors();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('HomeScreen'),
-        backgroundColor:  randColor(), //--------------------------------------
-        // myColor: non-flick,same for all, no setState change
-        // RandColor.color(): non-flick, same for all, no setState change
-        // randColor(): color-flick, different color for all, setState changes,
+        backgroundColor:  lc[0], //if randColor() function is used in all places, build(BuildContext context) is called twice and color changes twice.
       ),
       body: Center(
         child: Column(
@@ -44,18 +66,20 @@ class _HomeScreenState extends State<HomeScreen> {
             Container(
               width:300,
               height: 100,
-              color:  randColor(), // ----------------------------------------
+              color:  lc[1], //randColor() ----------------------------------------
             ),
             const SizedBox(height: 24,),
             Container(
               width:300,
               height: 100,
-              color:  randColor(), // ----------------------------------------
+              color:  lc[2], // randColor()----------------------------------------
             ),
             const SizedBox(height: 8,),
             ElevatedButton(
               onPressed: () {
-                setState(() {}); // appBar color is fixed!
+                setState(() { // ------------> does it call initState ? no?
+                  setColors(); ///
+                });
               },
               child: const Text('setState'),
             ),
@@ -64,15 +88,12 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
+// same effect if randColor() is written here.
 }
 
 Color randColor(){ //function
-  //WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-    Color myColor = Color((math.Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(0.8); //withOpacity(0.5)
-    return myColor;
-  // });
-  // return Colors.blue;
-
+  Color myColor = Color((math.Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(0.8); //withOpacity(0.5)
+  return myColor;
 }
 
 class RandColor{ // class
@@ -82,3 +103,7 @@ class RandColor{ // class
     return val;
   }
 }
+
+// myColor: non-flick,same for all, no setState change
+// RandColor.color(): non-flick, same for all, no setState change
+// randColor(): color-flick, different color for all, setState changes,
