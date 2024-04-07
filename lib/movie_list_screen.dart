fa3c71ea5353b35e48c865ec3ad980f149bbb31c
anup30 <anupbarua30@gmail.com>
2024-04-08 00:22:52 +0600

    firebase, movie_list 1.4

--- lib/movie_list_screen.dart
+++ lib/movie_list_screen.dart
@@ -38,35 +38,34 @@ class _MovieListScreenState extends State<MovieListScreen> {
         backgroundColor: Colors.blue,
       ),
       body: StreamBuilder(
-        stream: _firebaseFirestore.collection('movies').snapshots(),
-        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return Center(
-              child: Text(snapshot.error.toString()),
-            );
-          }
-
-          movieList.clear();
-          for (QueryDocumentSnapshot doc in (snapshot.data?.docs ?? [])) {
-            movieList.add(
-              Movie.fromJson(doc.id, doc.data() as Map<String, dynamic>),
-            );
-          }
-
-          return ListView.separated(
-            itemCount: movieList.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(movieList[index].name),
-                subtitle: Text(movieList[index].languages),
-                leading: Text(movieList[index].rating),
-                trailing: Text(movieList[index].year),
+          stream: _firebaseFirestore.collection('movies').snapshots(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(snapshot.error.toString()),
               );
-            },
-            separatorBuilder: (_, __) => const Divider(),
-          );
-        }
+            }
+            movieList.clear();
+            for (QueryDocumentSnapshot doc in (snapshot.data?.docs ?? [])) {
+              movieList.add(
+                Movie.fromJson(doc.id, doc.data() as Map<String, dynamic>),
+              );
+            }
+            return ListView.separated(
+              itemCount: movieList.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(movieList[index].name),
+                  subtitle: Text(movieList[index].languages),
+                  leading: Text(movieList[index].rating),
+                  trailing: Text(movieList[index].year),
+                );
+              },
+              separatorBuilder: (_, __) => const Divider(),
+            );
+          }
       ),
+
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
@@ -96,7 +95,6 @@ class _MovieListScreenState extends State<MovieListScreen> {
     );
   }
 }
-
 
 class Movie{  // model/pojo class
   final String id, name, languages, year, rating /*,earning*/;
