2469b551fe717a0637530b7a0f6d5bc2ee7db6ab
anup30 <anupbarua30@gmail.com>
2024-04-08 00:38:09 +0600

    firebase, movie_list 1.6

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 // app added to firebase project
 // guide: https://firebase.google.com/docs/flutter/setup?platform=ios
 
-import 'package:app01/movie_list_screen.dart';
+import 'package:app01/movie_list_at40.16.dart';
+//import 'package:app01/movie_list_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
@@ -21,7 +22,7 @@ class MoviesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: MovieListScreen(), // try, MovieListScreen4016()
+      home: MovieListScreen4016(), // try, MovieListScreen4016()
     );
   }
 }
